f3a269aaa71404eef38d59afe57b15b5a11fd113
qfj313659953 <qfj313659953@163.com>
2019-11-02 21:52:33 +0800

    博学谷的项目

---
+++ pages/index/index.js
@@ -0,0 +1,188 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    nav: [
+      {
+        img: "https://m.boxuegu.com/web/images/index/course.png",
+        txt: "就业课"
+      },
+      {
+        img: "https://m.boxuegu.com/web/images/index/mircoCourse.png",
+        txt: "提升课"
+      },
+      {
+        img: "https://m.boxuegu.com/web/images/index/freeMicroCourse.png",
+        txt: "免费课"
+      },
+      {
+        img: "https://m.boxuegu.com/web/images/index/courseType.png",
+        txt: "课程分类"
+      }
+    ],
+    list1:[
+      {
+        img:"https://file.boxuegu.com/f6c878722f394a7c9bd12c0319f00037.png",
+        title:"JavaEE",
+        cont:"JavaEE是一个开放的、基于标准的平台",
+        people:"1784人学习",
+        price:"￥14980.00"
+      },
+      {
+        img:"https://file.boxuegu.com/e19c53c6c1fa468aad6e11cb61d81b9a.png",
+        title:"Python+人工智能",
+        cont:"最有前景的开发职位",
+        people:"877人学习",
+        price:"￥14980.00"
+      },
+      {
+        img:"https://file.boxuegu.com/3940be1c755a44eeb8e2e0aaf984756e.png",
+        title:"前端与移动开发",
+        cont:"用户界面的构筑者",
+        people:"1230人学习",
+        price:"￥14980.00"
+      },
+      {
+        img:"https://file.boxuegu.com/6c0ffe21a06d421d90be8a0061c14c1a.png",
+        title:"软件测试",
+        cont:"",
+        people:"189人学习",
+        price:"￥12980.00"
+      }
+    ],
+    list2:[
+      {
+        img:"https://file.boxuegu.com/e6432db4d06c42b9aca7aede22ba9437.jpg",
+          title:"所有人都能学的数据分析课",
+          people:"710人学习",
+          price:"￥399.00"
+      },
+      {
+        img:"https://file.boxuegu.com/989b9bd2a12d471b8e7579b7182d08b6.png",
+          title:"MySQL数据库性能优化项目实战",
+          people:"107人学习",
+          price:"￥199.00"
+      },
+      {
+        img:"https://file.boxuegu.com/df25a3e7031a48d39a896d4e38d3d3d5.png",
+          title:"Java并发编程原理精讲",
+          people:"107人学习",
+          price:"￥199.00"
+      },
+      {
+        img:"https://file.boxuegu.com/854b60de9d6742dbb29a47370b7c8650.png",
+          title:"GAN生成对抗网络入门到精通",
+          people:"183人学习",
+          price:"￥199.00"
+      },
+      {
+        img:"https://file.boxuegu.com/746a8e78655a4b3ca97ccad5390fbc4b.png",
+          title:"基于Kylin搞定千亿级电信数据分析",
+          people:"187人学习",
+          price:"￥199.00"
+      },
+      {
+        img:"https://file.boxuegu.com/90bb08a6bacd49baa5492c5eaa94c060.jpg",
+          title:"游戏AI实战课—深入浅出tensorflow.js",
+          people:"109人学习",
+          price:"￥199.00"
+      }
+    ],
+    list3:[
+      {
+        img:"https://file.boxuegu.com/74773be265b840b3af9da88f6f42841d.jpg",
+          title:"2小时用Go语言实现人脸识别抖音甩狗头",
+          people:"770人学习"
+         
+      },
+      {
+        img:"https://file.boxuegu.com/7e1535f414a64d3fa4d60e7c8a84cca3.jpg",
+          title:"JavaEE在线就业班2.0(试听课)",
+          people:"316人学习"
+    
+      },
+      {
+        img:"https://file.boxuegu.com/3ed09c2ae4ea4542add4ac8b098c9535.jpg",
+          title:"Axure基础课",
+          people:"452人学习"
+         
+      },
+      {
+        img:"https://file.boxuegu.com/6a0330c2237b4ed3af2a2703cc2f1419.jpg",
+          title:"产品经理职业公开课",
+          people:"产品经理职业公开课"
+      },
+      {
+        img:"https://file.boxuegu.com/ccc47386e2b048658f944d0db943321c.jpg",
+          title:"短视频创造营探店精品课程",
+          people:"437人学习"
+        
+      },
+      {
+        img:"https://file.boxuegu.com/49b20b70e42e446cafc60f9c6fd98d84.jpg",
+          title:"剪辑大神50招",
+          people:"649人学习"
+      }
+    ],
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
